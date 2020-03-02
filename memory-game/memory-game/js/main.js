7e95bcbf06802107605c9eeacf616d9292036ca3
JaeChik <jaexus@hotmail.com>
2020-03-02 22:38:49 +0800

    created game logic

---
+++ memory-game/memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log ("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "King";
+let cardFour = "King";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
